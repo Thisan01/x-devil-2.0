f24419a997f04c295f81611e28bbf5674193bdf9
Ravana tech <85940832+Thisan01@users.noreply.github.com>
2021-10-03 11:54:31 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -1,7 +1,7 @@
 /* Copyright (C) 2020 Thisan01.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-DEVIL- Thisan01
+WhatsAsena- Thisan01
 */
 
 const chalk = require('chalk');
